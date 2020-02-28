41e8a38a8ba16c4d86b786ecbd20e6f68d5ab213
Mohammed Guniem <mghunime@yahoo.no>
2020-02-28 14:23:33 +0000

    Added script to solve Problem 3 and fixed the overall file structure

---
+++ statistics/destination_analyze/analyze_delay_based_on_destination_state.py
@@ -0,0 +1,40 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, collect_set, array_contains, size, first, sum as _sum, mean as _mean, desc, asc, count
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL to analyze delays based on destination state irrespective of route and origin") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+spark.sparkContext.setLogLevel('FATAL')
+
+df = spark.read.csv("data/2019_01.csv",header=True,sep=",")
+
+df = df.fillna( { 'CARRIER_DELAY':0,'WEATHER_DELAY':0,'NAS_DELAY':0,'SECURITY_DELAY':0,'LATE_AIRCRAFT_DELAY':0 } )
+
+showRowCount = 10
+
+dest_delay_df = df.groupBy('DEST_STATE_NM')
+
+print("Weather delay")
+dest_weather_delay = (dest_delay_df.agg(_sum('WEATHER_DELAY').alias("1"),_mean('WEATHER_DELAY').alias('2')).sort(desc("1"))).toDF('Destination State','Total Sum','Average')
+dest_weather_delay.show(showRowCount, False)
+
+print("Carrier delay")
+dest_carrier_delay = (dest_delay_df.agg(_sum('CARRIER_DELAY').alias("1"),_mean('CARRIER_DELAY').alias('2')).sort(desc("1"))).toDF('Destination State','Total Sum','Average')
+dest_carrier_delay.show(showRowCount, False)
+
+print("NAS delay")
+dest_nas_delay = (dest_delay_df.agg(_sum('NAS_DELAY').alias("1"),_mean('NAS_DELAY').alias('2')).sort(desc("1"))).toDF('Destination state','Total Sum','Average')
+dest_nas_delay.show(showRowCount, False)
+
+print("Security delay")
+dest_security_delay = (dest_delay_df.agg(_sum('SECURITY_DELAY').alias("1"),_mean('SECURITY_DELAY').alias('2')).sort(desc("1"))).toDF('Destination state','Total Sum','Average')
+dest_security_delay.show(showRowCount, False)
+
+print("Late aircraft delay")
+dest_late_aircraft_delay = (dest_delay_df.agg(_sum('LATE_AIRCRAFT_DELAY').alias("1"),_mean('LATE_AIRCRAFT_DELAY').alias('2')).sort(desc("1"))).toDF('Destination state','Total Sum','Average')
+dest_late_aircraft_delay.show(showRowCount, False)
+
+print("The total number of states in destination is: " + str(dest_weather_delay.count()))
