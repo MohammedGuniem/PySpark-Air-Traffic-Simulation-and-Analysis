d8abebb6f6daf416cdd928c5cbef2283cf3dcaad
Mohammed Guniem <mghunime@yahoo.no>
2020-03-21 14:37:25 +0000

    At this step, we have the alt., lon., and utc time variation of origin and destination.

--- simulation/simulate.py
+++ simulation/simulate.py
@@ -33,6 +33,21 @@ routes_df = routes_df.agg(_max('ORIGIN_AIRPORT_SEQ_ID').alias('ORIGIN_AIRPORT_SE
                           _mean('DISTANCE').alias('DISTANCE')).sort(desc("ROUTE_PATH"))
 print("Count of rows: ", routes_df.count())
 
+print("Adding latitude, longitude and UTC time to the origin and destination of each route...")
+airports_df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load("hdfs://master:9000/support_data/airports_data.csv")
+airports_df = airports_df.withColumnRenamed('AIRPORT_SEQ_ID','ORIGIN_AIRPORT_SEQ_ID')
+airports_df = airports_df.withColumn("DEST_AIRPORT_SEQ_ID", airports_df["ORIGIN_AIRPORT_SEQ_ID"])
+
+routes_df = routes_df.join(airports_df.select(*["ORIGIN_AIRPORT_SEQ_ID","LATITUDE","LONGITUDE","UTC_LOCAL_TIME_VARIATION"]), "ORIGIN_AIRPORT_SEQ_ID")
+routes_df = routes_df.withColumnRenamed('LATITUDE','ORIGIN_LATITUDE')
+routes_df = routes_df.withColumnRenamed('LONGITUDE','ORIGIN_LONGITUDE')
+routes_df = routes_df.withColumnRenamed('UTC_LOCAL_TIME_VARIATION','ORIGIN_UTC_LOCAL_TIME_VARIATION')
+
+routes_df = routes_df.join(airports_df.select(*["DEST_AIRPORT_SEQ_ID","LATITUDE","LONGITUDE","UTC_LOCAL_TIME_VARIATION"]), "DEST_AIRPORT_SEQ_ID")
+routes_df = routes_df.withColumnRenamed('LATITUDE','DEST_LATITUDE')
+routes_df = routes_df.withColumnRenamed('LONGITUDE','DEST_LONGITUDE')
+routes_df = routes_df.withColumnRenamed('UTC_LOCAL_TIME_VARIATION','DEST_UTC_LOCAL_TIME_VARIATION')
+print("Count of rows: ", routes_df.count())
 
 print("Writing csv file on local machine ...")
 routes_df.repartition(1).write.csv("debug_1", header = 'true')
