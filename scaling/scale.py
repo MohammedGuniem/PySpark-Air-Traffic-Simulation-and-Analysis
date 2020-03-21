9e92afd5fc10ac485fcccd3d836d3922c079c277
Mohammed Guniem <mghunime@yahoo.no>
2020-03-21 17:49:45 +0000

    Updating the old scaling folder

--- scaling/scale.py
+++ scaling/scale.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark import SparkContext, SparkConf
-from pyspark.sql.functions import split, udf
-from pyspark.sql.types import StringType, StructType, StructField
+from pyspark.sql.functions import split, udf, lit
+from pyspark.sql.types import StringType, ArrayType, StructType, StructField
 from pyspark.sql import SQLContext, Row
 from mpl_toolkits.basemap import Basemap
 import time
@@ -49,7 +49,19 @@ scaling_df = scaling_df.filter(scaling_df.WHEELS_ON.between(1720,1840))
 
 print("count after filtering between departure time and arrival time: ", scaling_df.count())
 
-def find_route_path(origin_lat, origin_lon, destination_lat, destination_lon, distance_in_miles, air_time_in_minutes, wheels_off):    
+schema = StructType([
+    StructField("ORIGIN_AIRPORT_SEQ_ID", StringType(), False),
+    StructField("DEST_AIRPORT_SEQ_ID", StringType(), False),
+    StructField("c", StringType(), False),
+    StructField("d", StringType(), False),
+    StructField("e", StringType(), False),
+    StructField("f", StringType(), False),
+    StructField("g", StringType(), False),
+    StructField("h", StringType(), False),
+    StructField("i", StringType(), False)
+])
+
+def find_route_path(origin_airport_id, destination_airport_id, origin_lat, origin_lon, destination_lat, destination_lon, distance_in_miles, air_time_in_minutes, wheels_off):
     area_map = Basemap(llcrnrlon=-109, llcrnrlat=37, urcrnrlon=-102, urcrnrlat=41, lat_ts=0, resolution='l')    
     Longs, Lats = area_map.gcpoints(origin_lon, origin_lat, destination_lon, destination_lat, (distance_in_miles/40)+1)
     
@@ -61,7 +73,7 @@ def find_route_path(origin_lat, origin_lon, destination_lat, destination_lon, di
     date_time = '2019-03-08 ' + str(wheels_off)
     pattern = '%Y-%m-%d %H%M'
     current_time = int(time.mktime(time.strptime(date_time, pattern)))
-
+    
     for lon in Longs:
         if float(lon) < -102 and float(lon) > -109 and float(Lats[Longs.index(lon)]) < 41 and float(Lats[Longs.index(lon)]) > 37:
            inside_lon_array.append(str(lon))
@@ -70,7 +82,7 @@ def find_route_path(origin_lat, origin_lon, destination_lat, destination_lon, di
         current_time += time_increase_rate
 
     if len(inside_lon_array) == 0 or len(inside_lat_array) == 0:
-       result = {'entry_lon': None, 'entry_lat': None, 'exit_lon': None, 'exit_lat': None, 'entry_time': None, 'exit_time': None, 'is_in_area': 'False'}
+       result = {'entry_lon': 'null', 'entry_lat': 'null', 'exit_lon': 'null', 'exit_lat': 'null', 'entry_time': 'null', 'exit_time': 'null', 'is_in_area': 'OUTSIDE'}
     else:    
        result = {'entry_lon': inside_lon_array[0],
                  'entry_lat': inside_lat_array[0],
@@ -78,12 +90,35 @@ def find_route_path(origin_lat, origin_lon, destination_lat, destination_lon, di
                  'exit_lat': inside_lat_array[len(inside_lat_array)-1],
                  'entry_time': str(time_array[0]),
                  'exit_time': str(time_array[len(time_array)-1]),
-                 'is_in_area': 'True'}
-    return json.dumps(result).replace(',','$')
+                 'is_in_area': 'INSIDE'}
+    #return [result['entry_lon'], result['entry_lat'], result['exit_lon'], result['exit_lat'], result['entry_time'], result['exit_time'], result['is_in_area']]
+    return (origin_airport_id, destination_airport_id, result['entry_lon'], result['entry_lat'], result['exit_lon'], result['exit_lat'], result['entry_time'], result['exit_time'], result['is_in_area'])
 
-udf_find_route_path = udf(find_route_path, StringType())
+#udf_find_route_path = udf(find_route_path, ArrayType(StringType()))
+#scaling_df = scaling_df.withColumn("ROUTE_PATH_INFO", udf_find_route_path("ORIGIN_LATITUDE", "ORIGIN_LONGITUDE", "DEST_LATITUDE", "DEST_LONGITUDE", "DISTANCE", "AIR_TIME", "WHEELS_OFF"))
+udf_find_route_path = udf(find_route_path, schema)
 
-scaling_df = scaling_df.withColumn('ROUTE_PATH_INFO', udf_find_route_path("ORIGIN_LATITUDE", "ORIGIN_LONGITUDE", "DEST_LATITUDE", "DEST_LONGITUDE", "DISTANCE", "AIR_TIME", "WHEELS_OFF"))
+route_info_df = scaling_df.select(udf_find_route_path("ORIGIN_AIRPORT_SEQ_ID", "DEST_AIRPORT_SEQ_ID", "ORIGIN_LATITUDE", "ORIGIN_LONGITUDE", "DEST_LATITUDE", "DEST_LONGITUDE", "DISTANCE", "AIR_TIME", "WHEELS_OFF").alias("i"))
+print("showing foobars")
+#route_info_df.select("ROUTE_INFO.ORIGIN_AIRPORT_SEQ_ID", "ROUTE_INFO.DEST_AIRPORT_SEQ_ID", "ROUTE_INFO.ENTRY_LONGITUDE", "ROUTE_INFO.ENTRY_LATITUDE", "ROUTE_INFO.EXIT_LONGITUDE", "ROUTE_INFO.EXIT_LATITUDE", "ROUTE_INFO.ENTRY_TIME", "ROUTE_INFO.EXIT_TIME", "ROUTE_INFO.IS_IN_AREA").show()
+
+route_info_df = route_info_df.select("i.ORIGIN_AIRPORT_SEQ_ID", "i.DEST_AIRPORT_SEQ_ID", "i.c", "i.d", "i.e", "i.f", "i.g", "i.h", "i.i")
+scaling_df = scaling_df.join(route_info_df.select(*["ORIGIN_AIRPORT_SEQ_ID"]), "ORIGIN_AIRPORT_SEQ_ID")
+scaling_df = scaling_df.join(route_info_df.select(*["DEST_AIRPORT_SEQ_ID", "c", "d", "e", "f", "g", "h", "i"]), "DEST_AIRPORT_SEQ_ID")
+
+
+#r = scaling_df["ROUTE_PATH_INFO"]
+"""
+scaling_df = scaling_df.withColumn("ENTRY_LONGITUDE", r.getItem(0))
+scaling_df = scaling_df.withColumn("ENTRY_LATITUDE", r.getItem(1))
+scaling_df = scaling_df.withColumn("EXIT_LONGITUDE", r.getItem(2)) 
+scaling_df = scaling_df.withColumn("EXIT_LATITUDE", r.getItem(3)) 
+scaling_df = scaling_df.withColumn("ENTRY_TIME", r.getItem(4)) 
+scaling_df = scaling_df.withColumn("EXIT_TIME", r.getItem(5)) 
+scaling_df = scaling_df.withColumn("IS_IN_AREA", r.getItem(6))
+
+scaling_df = scaling_df.withColumn("ENTRY_LONGITUDE", r.getItem(0)).withColumn("ENTRY_LATITUDE", r.getItem(1)).withColumn("EXIT_LONGITUDE", r.getItem(2)).withColumn("EXIT_LATITUDE", r.getItem(3)).withColumn("ENTRY_TIME", r.getItem(4)).withColumn("EXIT_TIME", r.getItem(5)).withColumn("IS_IN_AREA", r.getItem(6))
+"""
 
 print("count after determining the routes that belongs to the target area: ", scaling_df.count())
 
