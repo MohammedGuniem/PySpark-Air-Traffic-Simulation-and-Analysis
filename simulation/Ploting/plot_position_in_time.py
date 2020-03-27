0ca3ea4ed59b1e8352857c4b8a9e8ccc6288da99
Mohammed Zoher Guniem <2918474@uis.no>
2020-03-27 15:07:55 +0100

    Adding some plotting scripts to scaling and simulation

--- simulation/Ploting/plot_position_in_time.py
+++ simulation/Ploting/plot_position_in_time.py
@@ -12,8 +12,13 @@ m.drawcoastlines()
 
 with open("../simulated_data.json", 'r') as file:
     data = json.load(file)
+    entry_times = []
+    exit_times = []
+    filtered_routes = []
     for route in data:
-        if route['is_in_area'] == "INSIDE":
+        if route['is_in_area'] == "INSIDE" and \
+        (not (route['entry_time'] < 1558787340 and route['exit_time'] < 1558787340) and \
+        not (route['entry_time'] > 1558787341 and route['exit_time'] > 1558787341)):
             Points = {"Source":(route['dest_lat'],route['dest_lon']),"Destination":(route['origin_lat'],route['origin_lon'])}
             Lon = [Points[key][0] for key in Points]
             Lat = [Points[key][1] for key in Points]
@@ -25,6 +30,10 @@ with open("../simulated_data.json", 'r') as file:
             dest_lon = route['dest_lon']
             longs, lats = m.gcpoints(Lat[0],Lon[0],Lat[1],Lon[1],100)
             
+            entry_times.append(route['entry_time'])
+            exit_times.append(route['entry_time'])
+            filtered_routes.append(route)
+
             # drawing inside Colorado routes - orange
             if (dest_lon < -102 and dest_lon > -109 and dest_lat < 41 and dest_lat > 37) and (origin_lon < -102 and origin_lon > -109 and origin_lat < 41 and origin_lat > 37):
                 # drawing entry points - green
@@ -58,5 +67,11 @@ with open("../simulated_data.json", 'r') as file:
                 m.scatter(X,Y,zorder=5,s=50,color="#0000FF",marker="^")
                 plt.plot(longs,lats,color="#0000FF",linewidth=2)
 
+print(min(entry_times)) #1558787340
+print(max(entry_times)) #1558809554.7600906
+print(min(exit_times)) #1558787340
+print(max(exit_times)) #1558809554.7600906
 print("The number of flights over target area: ", len(data))
+print(len(filtered_routes))
 plt.show()
+
